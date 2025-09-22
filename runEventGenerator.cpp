beec2eafb80ea1e072db38cf8e082455f22a59ea
Leonel Martinez <98292196+lemartainz@users.noreply.github.com>
2025-09-22 12:04:16 -0400

    Add options for LUND output and plot generation
    Enhanced input handling to support 'write_lund' and 'gen_plots' options, allowing users to control LUND file output and plot generation via input.txt. Updated runEventGenerator.cpp to modularize event processing, enable/disable LUND file writing and plotting based on input, and improved debug output. Also updated .gitignore to exclude PDF files.

--- runEventGenerator.cpp
+++ runEventGenerator.cpp
@@ -58,6 +58,9 @@ struct ReadInput {
     double t_slope;
     std::string reaction;
     bool print_debug = false;
+    bool write_lund = true;
+    bool gen_plots = false;
+    std::string mass_X = "2.5"; // default mass of X in GeV
 };
 
 
@@ -114,6 +117,17 @@ ReadInput readInputFile(const string &filename) {
                 std::string val;
                 iss >> val;
                 input.print_debug = (val == "true" || val == "1");
+            } else if (key == "write_lund:") {
+                std::string val;
+                iss >> val;
+                input.write_lund = (val == "true" || val == "1");
+            } else if (key == "gen_plots:") {
+                std::string val;
+                iss >> val;
+                input.gen_plots = (val == "true" || val == "1");
+            } else if (key == "mass_X:") {
+                iss >> input.mass_X;
+
             } else {
                 cerr << "WARNING: unrecognized key '" << key << "' in " << filename << endl;
             }
@@ -351,145 +365,12 @@ void runEventGenerator() {
 
     cout << "Generated " << electronEvents.v_scattered.size() << " events passing W_min = " << input.W_min << endl;
 
-    // for (const auto& event : electronEvents.v_scattered) {
-    //     double theta = event.Theta();
-    //     h_e_theta->Fill(theta*TMath::RadToDeg());
-    //     cout << "Scattered electron theta (deg): " << theta * TMath::RadToDeg() << endl;
-    // }
-
-    // Example of a decay
-    vector<TLorentzVector> p1_lab_list; p1_lab_list.reserve(input.num_events);
-    vector<TLorentzVector> X_lab_list;  X_lab_list.reserve(input.num_events);
-    vector<TLorentzVector> p2_lab_list; p2_lab_list.reserve(input.num_events);
-    vector<TLorentzVector> pbar_lab_list; pbar_lab_list.reserve(input.num_events);
-
-    TH1D *h_t_gamma_k1 = new TH1D("h_t_gamma_k1", "t: virtual gamma - K1; t [GeV^{2}]; Counts", 100, 0, 6.0);
-    TH1D *h_t_p_X      = new TH1D("h_t_p_X",       "t: proton - X; t [GeV^{2}]; Counts", 100, 0, 6.0);
+    TH1D *h_t_gamma_k1 = new TH1D("h_t_gamma_k1", "t: #gamma^{*} - p; t [GeV^{2}]; Counts", 100, 0, 6.0);
+    TH1D *h_t_p_X      = new TH1D("h_t_p_X",       "t: p_{t} - X; t [GeV^{2}]; Counts", 100, 0, 6.0);
+    TH1D *h_m_X        = new TH1D("h_m_X",         "Invariant Mass of X; M_{X} [GeV]; Counts", 100, 0, 6.0);
+    TH1D *h_m_X_calc   = new TH1D("h_m_X_calc",    "Calculated Invariant Mass of X from daughters; M_{X} [GeV]; Counts", 100, 0, 6.0);
 
     cout << "Processing decays..." << endl;
-    // double mp = PDG::proton, mpbar = PDG::proton;
-    // for(size_t i=0;i<electronEvents.v_W.size();i++){
-    //     double mX = 2.5;
-    //     if (mX <= (mp + mpbar)) continue;
-
-    //     auto decay1 = gen.twoBodyDecayWeighted(electronEvents.v_W[i], mp, mX, input.t_slope, electronEvents.v_virtual[i]);
-    //     TLorentzVector p_p1_lab = decay1.d1_lab;
-    //     TLorentzVector p_X_lab  = decay1.d2_lab;
-
-    //     if (p_X_lab.E() == 0 && p_p1_lab.P() == 0) continue;
-    //     double t_min = decay1.t_min;
-    //     double t_max = decay1.t_max;
-
-
-    //     auto decay2 = gen.twoBodyDecayWeighted(p_X_lab, mp, mpbar, 0, electronEvents.v_virtual[i]);
-    //     TLorentzVector p_p2_lab = decay2.d1_lab;
-    //     TLorentzVector p_pbar_lab = decay2.d2_lab;
-
-    //     if (p_p2_lab.E() == 0 && p_pbar_lab.E() == 0) continue;
-
-    //     p1_lab_list.push_back(p_p1_lab);
-    //     X_lab_list.push_back(p_X_lab);
-    //     p2_lab_list.push_back(p_p2_lab);
-    //     pbar_lab_list.push_back(p_pbar_lab);
-
-    //     double t1 = invariantSquare(electronEvents.v_virtual[i], p_p1_lab, false);
-    //     double t2 = invariantSquare(p_target, p_X_lab, false);
-
-    //     h_t_gamma_k1->Fill(-(t1-t_max));
-    //     h_t_p_X->Fill(-(t2-t_max));
-
-    // }
-    // cout << "Finished processing decays." << endl;
-
-    // cout << "Creating LUND file..." << endl;
-    // // Write to LUND file
-    // ofstream fout("events.lund");
-    // if (!fout.is_open()) {
-    //     cerr << "ERROR: cannot open events.lund for writing." << endl;
-    // } else {
-    //     int nEvents = (int)p1_lab_list.size();
-    //     for (int i=0;i<nEvents;++i) {
-    //         int num_particles = 4;
-    //         fout << "\t" << num_particles << " 1 1 0 0 11 " << input.beam_energy << " 2212 " << PDG::proton << " 0\n";
-    //         double vz_rand = gen.rnd.Uniform(-5.0, 0.0);
-    //         // 1) p1
-    //         {
-    //             int j = 1;
-    //             int pid = 2212; // proton
-    //             TLorentzVector &v = p1_lab_list[i];
-    //             fout << j << " " << 0 << " " << 1 << " " << pid << " 0 0 ";
-    //             fout << std::fixed << std::setprecision(6)
-    //                  << v.Px() << " " << v.Py() << " " << v.Pz() << " " << v.E() << " "
-    //                  << mp << " " << vz_rand << " " << 0.0 << "\n";
-    //         }
-    //         // 2) pbar
-    //         {
-    //             int j = 2;
-    //             int pid = -2212; // antiproton
-    //             TLorentzVector &v = pbar_lab_list[i];
-    //             fout << j << " " << 0 << " " << 1 << " " << pid << " 0 0 ";
-    //             fout << std::fixed << std::setprecision(6)
-    //                  << v.Px() << " " << v.Py() << " " << v.Pz() << " " << v.E() << " "
-    //                  << mpbar << " " << vz_rand << " " << 0.0 << "\n";
-    //         }
-    //         // 3) p2
-    //         {
-    //             int j = 3;
-    //             int pid = 2212; // proton
-    //             TLorentzVector &v = p2_lab_list[i];
-    //             fout << j << " " << 0 << " " << 1 << " "    << pid << " 0 0 ";
-    //             fout << std::fixed << std::setprecision(6)
-    //                  << v.Px() << " " << v.Py() << " " << v.Pz() << " " << v.E() << " "
-    //                  << mp << " " << vz_rand << " " << 0.0 << "\n";
-    //         }
-    //         // 4) scattered electron
-    //         {
-    //             int j = 4;
-    //             int pid = 11;
-    //             TLorentzVector &v = electronEvents.v_scattered[i];
-    //             fout << j << " " << 0 << " " << 1 << " " << pid << " 0 0 ";
-    //             fout << std::fixed << std::setprecision(6)
-    //                  << v.Px() << " " << v.Py() << " " << v.Pz() << " " << v.E() << " "
-    //                  << PDG::electron << " " << vz_rand << " " << 0.0 << "\n";
-    //         }
-    //         fout << "\n";
-    //     }
-    //     fout.close();
-    //     cout << "Written events.lund" << endl;
-    // }
-
-    // double fit_min = 0.;
-    // double fit_max = 6.0;
-    // TF1 *f_exp1 = new TF1("f_exp1", "[0]*exp(-[1]*x)", fit_min, fit_max);
-    // f_exp1->SetParameters(1000, 2.0);
-    // // h_t1prime->Fit(f_exp1, "RQ", "", fit_min, fit_max);
-
-    // TF1 *f_exp_t1raw = new TF1("f_exp_t1raw", "[0]*exp(-[1]*x)", 0.0, 3.0);
-    // f_exp_t1raw->SetParameters(f_exp1->GetParameter(0), f_exp1->GetParameter(1));
-
-    // TF1 *f_exp_gamma_k1 = new TF1("f_exp_gamma_k1", "[0]*exp(-[1]*x)", fit_min, fit_max);
-    // f_exp_gamma_k1->SetParameters(100, 2.0);
-    // h_t_gamma_k1->Fit(f_exp_gamma_k1, "RQ", "", fit_min, fit_max);
-
-    // TF1 *f_exp_p_X = new TF1("f_exp_p_X", "[0]*exp(-[1]*x)", fit_min, fit_max);
-    // f_exp_p_X->SetParameters(100, 2.0);
-    // h_t_p_X->Fit(f_exp_p_X, "RQ ", "", fit_min, fit_max);
-
-    // TCanvas *c1 = new TCanvas("c1","t distributions",1200,500);
-    // c1->Divide(2,1);
-    // c1->cd(1);
-    // h_t_gamma_k1->Draw();
-    // f_exp_gamma_k1->SetLineColor(kRed);
-    // f_exp_gamma_k1->Draw("same");
-    // c1->cd(2);
-    // h_t_p_X->Draw();
-    // f_exp_p_X->SetLineColor(kRed);
-    // f_exp_p_X->Draw("same");
-    // c1->Update();
-
-    // TCanvas *c2 = new TCanvas("c2","t distributions log",1200,500);
-    // h_e_theta->Draw();
-    // c2->Update();
 
     // Parse the decay map from the reaction string in your input file
     auto parents = getFirstDecayDaughters(input.reaction);
@@ -514,6 +395,8 @@ void runEventGenerator() {
             cout << endl;
     }
     std::vector<std::vector<std::pair<int, TLorentzVector>>> all_final_particles;
+    std::vector<double> all_t_max;
+    std::vector<double> all_t_min;
     // // For each event:
     for(size_t i=0; i<electronEvents.v_W.size(); i++) {
         std::vector<std::pair<int, TLorentzVector>> final_particles;
@@ -530,85 +413,157 @@ void runEventGenerator() {
         double m1 = getMass(pdg1);
         double m2 = getMass(pdg2);
 
+        // TF1 *mass_func = new TF1("mass_X_func", input.mass_X.c_str(), 0, (p_beam + p_target).M());
+        // double mass_X = mass_func->GetRandom();
+
         // First decay: W -> pdg1 + pdg2
         auto decay1 = gen.twoBodyDecayWeighted(electronEvents.v_W[i], m1, m2, input.t_slope, electronEvents.v_virtual[i]);
         TLorentzVector p1_lab = decay1.d1_lab;
         TLorentzVector p2_lab = decay1.d2_lab;
+        double t_max = decay1.t_max;
+        double t_min = decay1.t_min;
 
         // Recursively decay both daughters
         performDecay(p1_lab, pdg1, decay_map, gen, final_particles, electronEvents.v_virtual[i], 0);
         performDecay(p2_lab, pdg2, decay_map, gen, final_particles, electronEvents.v_virtual[i], 0);
         final_particles.emplace_back(11, electronEvents.v_scattered[i]); // Add scattered electron
         all_final_particles.push_back(final_particles);
+        all_t_max.push_back(t_max);
+        all_t_min.push_back(t_min);
         if (input.print_debug) {
-            // Print out Initial and Intermediate states for verification
-            std::cout << "Sum Intermediate State Daughters: (Px, Py, Pz, E): ("
-                    << p1_lab.Px() + p2_lab.Px() << ", "
-                    << p1_lab.Py() + p2_lab.Py() << ", "
-                    << p1_lab.Pz() + p2_lab.Pz() << ", "
-                    << p1_lab.E()  + p2_lab.E()  << ")\n";
-            
-            std::cout << "Sum Initial State: (Px, Py, Pz, E): ("
-                    << electronEvents.v_W[i].Px() << ", " << electronEvents.v_W[i].Py() << ", "
-                    << electronEvents.v_W[i].Pz() << ", " << electronEvents.v_W[i].E() << ")\n";
+            // Print out modular method debug output in the same structure as the old method
+            std::cout << "[MODULAR] Event " << i << ":\n";
+            std::cout << "  W:    (" << electronEvents.v_W[i].Px() << ", " << electronEvents.v_W[i].Py() << ", " << electronEvents.v_W[i].Pz() << ", " << electronEvents.v_W[i].E() << ")\n";
+            std::cout << "  p1:   (" << p1_lab.Px() << ", " << p1_lab.Py() << ", " << p1_lab.Pz() << ", " << p1_lab.E() << ") [PDG: " << pdg1 << "]\n";
+            std::cout << "  p1_test: (" << final_particles[0].second.Px() << ", " << final_particles[0].second.Py() << ", " << final_particles[0].second.Pz() << ", " << final_particles[0].second.E() << ") [PDG: " << final_particles[0].first << "]\n";
+            std::cout << "  X:   (" << p2_lab.Px() << ", " << p2_lab.Py() << ", " << p2_lab.Pz() << ", " << p2_lab.E() << ") [PDG: " << pdg2 << "]\n";
 
-            TLorentzVector daughter1_lab = final_particles[1].second; // Assuming first final particle is daughter1
-            TLorentzVector daughter2_lab = final_particles[2].second; // Assuming second final particle is daughter2
-            TLorentzVector sum_daughters = daughter1_lab + daughter2_lab;
-
-            // Print out the Intermediate state and sum of daughters
-            std::cout << "Parent 9999: (Px, Py, Pz, E): ("
-                    << p2_lab.Px() << ", " << p2_lab.Py() << ", " << p2_lab.Pz() << ", " << p2_lab.E() << ")\n";
-            std::cout << "Sum daughters: (Px, Py, Pz, E): ("
-                    << sum_daughters.Px() << ", " << sum_daughters.Py() << ", "
-                    << sum_daughters.Pz() << ", " << sum_daughters.E() << ")\n";
-        }
-        // Now final_particles contains all final state particles for this event
-        if (input.print_debug) {
-            std::cout << "Event " << i+1 << " final state particles:" << std::endl;
-            for (const auto& p : final_particles) {
-                std::cout << "  PDG: " << p.first << ", (Px, Py, Pz, E): (" 
-                          << p.second.Px() << ", " << p.second.Py() << ", " 
-                          << p.second.Pz() << ", " << p.second.E() << ")" << std::endl;
+            // If the decay map contains 9999, print its daughters
+            auto it = decay_map.find(9999);
+            if (it != decay_map.end() && it->second.size() == 2) {
+                // Find the TLorentzVectors for the daughters of 9999
+                TLorentzVector daughter1_lab, daughter2_lab;
+                daughter1_lab = final_particles[1].second; // Assuming first daughter is first in final_particles
+                daughter2_lab = final_particles[2].second; // Assuming second daughter is second in final_particles
+                std::cout << "  p2:   (" << daughter1_lab.Px() << ", " << daughter1_lab.Py() << ", " << daughter1_lab.Pz() << ", " << daughter1_lab.E() << ") [PDG: " << it->second[0] << "]\n";
+                std::cout << "  pbar: (" << daughter2_lab.Px() << ", " << daughter2_lab.Py() << ", " << daughter2_lab.Pz() << ", " << daughter2_lab.E() << ") [PDG: " << it->second[1] << "]\n";
             }
+
+            std::cout << "  e-:   (" << electronEvents.v_scattered[i].Px() << ", " << electronEvents.v_scattered[i].Py() << ", " << electronEvents.v_scattered[i].Pz() << ", " << electronEvents.v_scattered[i].E() << ")\n";
         }
 
     }
 
     cout << "Finished processing decays." << endl;
     // Write to LUND file as necessary...
-    // if (input.write_lund) {
+    if (input.write_lund) {
         cout << "Creating LUND file..." << endl;
-        ofstream fout("events.lund");
-        if (!fout.is_open()) {
+        ofstream fout2("events.lund");
+        if (!fout2.is_open()) {
             cerr << "ERROR: cannot open events.lund for writing." << endl;
         } else {
             int nEvents = (int)electronEvents.v_W.size();
             for (int i=0;i<nEvents;++i) {
                 // Assuming final_particles is available here
-                int num_particles = all_final_particles[i].size() + 1; // +1 for scattered electron
-                fout << "\t" << num_particles << " 1 1 0 0 11 " << input.beam_energy << " 2212 " << PDG::proton << " 0\n";
+                int num_particles = all_final_particles[i].size(); // +1 for scattered electron
+                fout2 << "\t" << num_particles << " 1 1 0 0 11 " << input.beam_energy << " 2212 " << PDG::proton << " 0\n";
                 double vz_rand = gen.rnd.Uniform(-5.0, 0.0);
                 
                 // Write final state particles
                 for (size_t j = 0; j < all_final_particles[i].size(); ++j) {
                     int pid = all_final_particles[i][j].first;
                     TLorentzVector &v = all_final_particles[i][j].second;
-                    fout << j+1 << " " << 0 << " " << 1 << " " << pid << " 0 0 ";
-                    fout << std::fixed << std::setprecision(6)
+                    fout2 << j+1 << " " << 0 << " " << 1 << " " << pid << " 0 0 ";
+                    fout2 << std::fixed << std::setprecision(6)
                          << v.Px() << " " << v.Py() << " " << v.Pz() << " " << v.E() << " "
                          << getMass(pid) << " " << vz_rand << " " << 0.0 << "\n";
                 }
                 
-                fout << "\n";
+                fout2 << "\n";
             }
-            fout.close();
+            fout2.close();
             cout << "Written events.lund" << endl;
         }
-    // } else {
-        // cout << "Skipping LUND file creation." << endl;
-    // }
+    } else {
+        cout << "Skipping LUND file creation." << endl;
+    }
+    // Generate plots if requested
+    if (input.gen_plots) {
+        for (const auto& event : electronEvents.v_scattered) {
+            double theta = event.Theta();
+            h_e_theta->Fill(theta*TMath::RadToDeg());
+        }
+        for (size_t i = 0; i < electronEvents.v_W.size(); ++i) {
+            TLorentzVector p_W = electronEvents.v_W[i];
+            TLorentzVector p_scattered = electronEvents.v_scattered[i];
+            TLorentzVector p_virtual = electronEvents.v_virtual[i];
+            TLorentzVector p_proton = p_target; // Target proton at rest
+            TLorentzVector p_electron = all_final_particles[i].back().second; // Last particle is scattered electron
+            TLorentzVector p_p1 = all_final_particles[i][0].second; // First particle from W decay
+            TLorentzVector p_p2 = all_final_particles[i][1].second; // First particle from X decay
+            TLorentzVector p_pbar = all_final_particles[i][2].second; // Second particle from X decay
+            TLorentzVector p_X = p_p2 + p_pbar; // Assuming X decays to p1, p2, pbar
+            double t_p_X = invariantSquare(p_proton, p_X, false);
+            double t_gamma_p1 = invariantSquare(p_virtual, p_p1, false);
+            double m_X = p_X.M();
+            double m_X_calc = (p_p2 + p_pbar).M();
+            double t_max = all_t_max[i];
+            double t_min = all_t_min[i];
 
+            h_m_X->Fill(m_X);
+            h_m_X_calc->Fill(m_X_calc);
+            h_t_p_X->Fill(-(t_p_X - t_max));
+            h_t_gamma_k1->Fill(-(t_gamma_p1 - t_max));
+
+        }
+
+        cout << "Generating plots..." << endl;
+        TCanvas *c1 = new TCanvas("c1", "Scattered Electron Theta", 800, 600);
+        h_e_theta->Draw();
+        // c1->SaveAs("scattered_electron_theta.png");
+        // delete c1;
+
+        TCanvas *c2 = new TCanvas("c2", "Momentum Transfer", 800, 600);
+        TF1 *fit_t = new TF1("fit_t", "[0]*exp(-[1]*x)", 0, 6.0);
+        fit_t->SetParameters(1.0, 1.0);
+        h_t_gamma_k1->SetLineColor(kRed);
+        // h_t_gamma_k1->SetLineStyle(2);
+        h_t_gamma_k1->SetFillStyle(3001);
+        h_t_gamma_k1->SetFillColor(kRed);
+        h_t_gamma_k1->Draw();
+        h_t_gamma_k1->Fit(fit_t, "R");
+        fit_t->SetLineColor(kBlue);
+        fit_t->Draw("SAME");
+        TLegend *leg = new TLegend(0.6, 0.7, 0.9, 0.9);
+        leg->AddEntry(h_t_gamma_k1, "Data", "lep");
+        leg->AddEntry(h_t_p_X, "Data", "lep");
+
+        leg->AddEntry(fit_t, TString::Format("Fit: B=%.3f±%.3f",
+                                    fit_t->GetParameter(1), fit_t->GetParError(1)), "l");
+        // leg->AddEntry(fit_t, oss.str().c_str(), "l");
+        leg->Draw();
+        h_t_gamma_k1->SetStats(0);
+        h_t_p_X->SetStats(0);
+        c2->Update();
+
+        // c2->cd(2);
+        
+        h_t_p_X->Draw("SAMEE");
+        // c2->SaveAs("momentum_transfer.png");
+        // delete c2;
+
+        TCanvas *c3 = new TCanvas("c3", "Invariant Mass of X", 800, 600);
+        // c3->Divide(2,1);
+        // c3->cd(1);
+        // h_m_X->Draw();
+        // c3->cd(2);
+        h_m_X_calc->SetLineColor(kRed);
+        h_m_X_calc->SetLineStyle(2);
+        h_m_X_calc->SetFillStyle(3001);        
+        h_m_X_calc->Draw();
+        h_m_X->Draw("SAMEE");
+
+    }
     cout << "Event generation complete." << endl;
     
 }
